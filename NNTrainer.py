80a49b80ff0ac3215179191a48b3c25782d0ff6d
Brady Neal <bradyneal11@gmail.com>
2018-01-12 14:31:21 -0500

    Move models into seperate module from NNTrainer and add simpler models

--- NNTrainer.py
+++ NNTrainer.py
@@ -7,14 +7,16 @@ import torch.optim as optim
 from torchvision import datasets, transforms
 from torch.autograd import Variable
 
+
 class NNTrainer:
     """
     Class for training neural networks and outputing various measures of the
     information the weights contain.
     """
 
-    def __init__(self, batch_size=64, test_batch_size=1000, epochs=10, lr=0.01, 
-               momentum=0.5, no_cuda=False, seed=False, log_interval=100):
+    def __init__(self, model, batch_size=64, test_batch_size=1000, epochs=10,
+                 lr=0.01, momentum=0.5, no_cuda=False, seed=False,
+                 log_interval=100):
         self.batch_size = batch_size
         self.test_batch_size = test_batch_size
         self.epochs = epochs
@@ -34,7 +36,7 @@ class NNTrainer:
                 torch.cuda.manual_seed(seed)
 
         # Create network and optimizer
-        self.model = Net()
+        self.model = model
         if self.cuda:
             self.model.cuda()
         self.optimizer = optim.SGD(self.model.parameters(), lr=lr, momentum=momentum)
@@ -105,24 +107,3 @@ class NNTrainer:
             100. * num_correct / len(self.test_loader.dataset)))
         if return_correct:
             return correct
-        
-
-class Net(nn.Module):
-    """Neural network used in NNTrainer"""
-    
-    def __init__(self):
-        super(Net, self).__init__()
-        self.conv1 = nn.Conv2d(1, 10, kernel_size=5)
-        self.conv2 = nn.Conv2d(10, 20, kernel_size=5)
-        self.conv2_drop = nn.Dropout2d()
-        self.fc1 = nn.Linear(320, 50)
-        self.fc2 = nn.Linear(50, 10)
-
-    def forward(self, x):
-        x = F.relu(F.max_pool2d(self.conv1(x), 2))
-        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2))
-        x = x.view(-1, 320)
-        x = F.relu(self.fc1(x))
-        x = F.dropout(x, training=self.training)
-        x = self.fc2(x)
-        return F.log_softmax(x)
