e57b520e4d0fe0650d25bacc61c404429e3503a7
Brady Neal <bradyneal11@gmail.com>
2018-01-24 15:00:41 -0500

    Add plotting of all of the linear loss visualizations to a single plot

--- exp_inter_loss.py
+++ exp_inter_loss.py
@@ -9,12 +9,17 @@ import matplotlib as mpl
 mpl.use('Agg')
 import matplotlib.pyplot as plt
 
-HIDDEN_SIZES = [10, 15, 25, 50, 100, 250, 500]
-# HIDDEN_SIZES = [5, 10, 15, 25, 50, 100, 250, 500]
+NUM_PAIRS = 25
+# HIDDEN_SIZES = [10, 50]
+HIDDEN_SIZES = [5, 10, 15, 25, 50, 100, 250, 500]
 MODELS_DIR = '/data/milatmp1/nealbray/information-paths/saved/models'
 LINEAR_FIG_DIR = '/data/milatmp1/nealbray/information-paths/figures'
 
-def plot_linearization(model1, model2, num_hidden, pair_i):
+def hidden_size_to_color(num_hidden):
+    return 'C' + str(HIDDEN_SIZES.index(num_hidden))
+
+
+def plot_linearization(model1, model2, num_hidden, pair_i, same_plot=True):
     num_steps = 10
     losses = []
     for i in range(num_steps):
@@ -23,14 +28,18 @@ def plot_linearization(model1, model2, num_hidden, pair_i):
         inter_loss = tester.evaluate_loss().data[0]
         print(inter_loss)
         losses.append(inter_loss)
-        
-    plt.figure()
-    plt.title('num_hidden: %d' % num_hidden)
-    plt.plot(range(num_steps), losses)
-    filename = os.path.join(LINEAR_FIG_DIR, 'linear%d_%d.png' % (num_hidden, pair_i))
-    print('Saving', filename)
-    plt.savefig(filename)
-    plt.close()     # Close figure for memory reasons
+    
+    if not same_plot:
+        plt.figure()
+        plt.title('num_hidden: %d\trun: %d' % (num_hidden, pair_i))
+        plt.plot(range(num_steps), losses)
+        filename = os.path.join(LINEAR_FIG_DIR, 'linear%d_%d.png' % (num_hidden, pair_i))
+        print('Saving', filename)
+        plt.savefig(filename)
+        plt.close()     # Close figure for memory reasons
+    else:
+        plt.plot(range(num_steps), losses, hidden_size_to_color(num_hidden))
+        # filename = os.path.join(LINEAR_FIG_DIR, 'linear%d_%d.png' % (num_hidden, pair_i))
 
 
 def get_filename(num_hidden, i, slurm_id):
@@ -53,15 +62,22 @@ def get_models(num_hidden):
     return models
 
 
-def plot_linearizations(num_hidden):
+def plot_linearizations(num_hidden, same_plot=True):
     models = get_models(num_hidden)
     all_pairs = list(combinations(models, 2))
-    rand_pairs = random.sample(all_pairs, 25)
+    rand_pairs = random.sample(all_pairs, NUM_PAIRS)
     for i, (model1, model2) in enumerate(rand_pairs):
-        plot_linearization(model1, model2, num_hidden, i)
-    plt.close('all')    # Make sure all figures are closed for memory reasons
-        
+        plot_linearization(model1, model2, num_hidden, i, same_plot=same_plot)
+
+
+def plot_all_linearizations():
+    for num_hidden in HIDDEN_SIZES:
+        plot_linearizations(num_hidden, same_plot=True)
+    filename = os.path.join(LINEAR_FIG_DIR, 'linear_all.png')
+    plt.savefig(filename)
+    
         
 if __name__ == '__main__':
-    for num_hidden in HIDDEN_SIZES:
-        plot_linearizations(num_hidden)
+    # for num_hidden in HIDDEN_SIZES:
+    #     plot_linearizations(num_hidden, same_plot=False)
+    plot_all_linearizations()
