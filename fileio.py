8dd72ec75da7f115220c79a2f58c686d4de02bdc
Vinayak Tantia <tantia.vinayak1@gmail.com>
2018-03-07 15:49:44 -0500

    Read username using getpass

--- fileio.py
+++ fileio.py
@@ -6,9 +6,10 @@ from __future__ import print_function, division
 import os
 import re
 import torch
+import getpass
 
-
-OUTPUT_DIR = '/data/milatmp1/nealbray/information-paths/'
+USERNAME = getpass.getuser()
+OUTPUT_DIR = os.path.join('/data/milatmp1/', USERNAME, '/information-paths/')
 SAVED_DIR = os.path.join(OUTPUT_DIR, 'saved')
 MODEL_DIR = os.path.join(SAVED_DIR, 'models')
 TRAIN_BITMAP_DIR =  os.path.join(SAVED_DIR, 'train_bitmaps')
@@ -34,7 +35,7 @@ def save_weights(weights, num_hidden, i, slurm_id):
 
 def save_train_bitmap(bitmap, num_hidden, i, slurm_id):
     return torch.save(bitmap, get_train_bitmap_path(num_hidden, i , slurm_id))
-    
+
 def save_test_bitmap(bitmap, num_hidden, i, slurm_id):
     return torch.save(bitmap, get_test_bitmap_path(num_hidden, i, slurm_id))
 
@@ -59,7 +60,7 @@ def load_weights(num_hidden, i , slurm_id):
 
 def load_train_bitmap(num_hidden, i , slurm_id):
     return load_torch(get_train_bitmap_path(num_hidden, i , slurm_id))
-    
+
 def load_test_bitmap(num_hidden, i , slurm_id):
     return load_torch(get_test_bitmap_path(num_hidden, i , slurm_id))
 
@@ -87,8 +88,8 @@ def load_torch(filename, to_cpu=TO_CPU_DEFAULT):
         # likely CUDA error from saving it from GPU and loading to CPU
         except RuntimeError as e:
             return load_to_cpu(filename)
-            
-    
+
+
 def load_to_cpu(filename):
     """Load torch object specifically to CPU"""
     return torch.load(filename, map_location=lambda storage, loc: storage)
@@ -99,23 +100,23 @@ def load_model_information():
     TODO: trip this down and finish this (currently a copy paste)
     Load model information from disk
     """
-    
+
     bitmap = torch.load(os.path.join(SAVED_BITMAP_DIR, load_fn))
     weight_vec = torch.load(os.path.join(SAVED_WEIGHTS_DIR, load_fn))
-    
+
     save_model_filename = COMMON_NAMING_FORMAT % (num_hidden, i, slurm_id)
     save_info_fn = save_model_fn
     if load_model_inf or load_models:
         load_fn = COMMON_NAMING_FORMAT % (num_hidden, i, saved_slurm_id)
         save_info_fn = load_fn
-        
+
     # Common file naming
         save_model_fn = 'shallow%d_run%d_job%s.pt' % (num_hidden, i, slurm_id)
         save_info_fn = save_model_fn
         if load_model_inf or load_models:
             load_fn = 'shallow%d_run%d_job%s.pt' % (num_hidden, i, saved_slurm_id)
             save_info_fn = load_fn
-        
+
         # Load model information (fastest)
         if load_model_inf:
             bitmap = torch.load(os.path.join(SAVED_BITMAP_DIR, load_fn))
@@ -137,8 +138,8 @@ def load_model_information():
             weight_vec = shallow_net.get_params()
             torch.save(bitmap, os.path.join(SAVED_BITMAP_DIR, save_info_fn))
             torch.save(weight_vec, os.path.join(SAVED_WEIGHTS_DIR, save_info_fn))
-        
-        # Append bitmaps and weights to output lists    
+
+        # Append bitmaps and weights to output lists
         bitmaps.append(bitmap)
         weights.append(weight_vec)
 
