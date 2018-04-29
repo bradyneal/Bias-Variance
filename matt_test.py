eef4c5f900497ceae9a3aa80d6e83365f9c3d146
mattscicluna <mattcscicluna@gmail.com>
2018-04-29 15:30:27 -0400

    changed dir again, should make running on cedar easier

--- matt_test.py
+++ matt_test.py
@@ -14,11 +14,11 @@ run_exp_e = True
 
 import getpass
 
-USERNAME = getpass.getuser()
-OUTPUT_DIR = os.path.join('/data/milatmp1', USERNAME, 'information-paths')
+#USERNAME = getpass.getuser()
+#OUTPUT_DIR = os.path.join('/data/milatmp1', USERNAME, 'information-paths')
 
 #  Local changes only
-#OUTPUT_DIR = os.path.join(os.getcwd(), 'matt_folder')
+OUTPUT_DIR = os.path.join(os.getcwd(), 'matt_folder')
 
 #  Learning Curves
 if run_exp_a:
