07204cb6f03202396dd90631422357a63fe4fdc7
Vinayak Tantia <tantia.vinayak1@gmail.com>
2018-05-03 17:28:29 -0400

    Add code to calculate weight norm

--- models.py
+++ models.py
@@ -2,16 +2,19 @@ from __future__ import print_function
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+import numpy as np
 from copy import deepcopy
 
 MNIST_D = 784   # 28 x 28
 MNIST_K = 10
+
 
 def get_inter_model(model1, model2, theta):
     inter_model = deepcopy(model1)
     for p1, p2, inter_p in zip(model1.parameters(), model2.parameters(), inter_model.parameters()):
         inter_p.data = theta * p1.data + (1 - theta) * p2.data
     return inter_model
+
 
 class nn_custom_super(nn.Module):
 
@@ -49,6 +52,11 @@ class ShallowNet(nn_custom_super):
         out = self.fc2(h)
         return F.log_softmax(out)
 
+    def get_weight_norms(self, p=2):
+        nn_linears = [self.fc1, self.fc2]
+        weights_arr = [next(nn_linear.parameters()) for nn_linear in nn_linears]
+        weight_norms = [weights.norm(p) for weights in weights_arr]
+        return weight_norms
 
 
 class MinDeepNet(nn_custom_super):
@@ -63,7 +71,7 @@ class MinDeepNet(nn_custom_super):
     def forward(self, x):
         x = x.view(-1, MNIST_D)
         h1 = F.relu(self.fc1(x))
-        h2= F.relu(self.fc2(h1))
+        h2 = F.relu(self.fc2(h1))
         out = self.fc3(h2)
         return F.log_softmax(out)
 
