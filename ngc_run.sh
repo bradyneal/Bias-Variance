629abef51c212e2d20017a516fc9b60b403a745e
Vinayak Tantia <tantia.vinayak1@gmail.com>
2018-05-07 19:43:44 -0400

    Add code for running multiple jobs on NVIDIA in parallel

---
+++ ngc_run.sh
@@ -0,0 +1,14 @@
+echo "Here"
+echo "$@"
+
+cmd="python3 ngc_run.py $@"
+
+ngc batch run \
+  --instance ngcv1 \
+  --name "Test job" \
+  --image "mila1234/tantiavi-pytorch-volta:1" \
+  --result /data/milatmp1/root/information-paths/saved/ \
+  --command "git clone git@github.com:bradyneal/information-paths.git; \
+    cd information-paths; \
+    pip install -r requirements.txt; \
+    $cmd"
