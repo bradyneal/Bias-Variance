f63aadefe30f69ffe03afde3b240256f221d8cc3
Brady Neal <bradyneal11@gmail.com>
2018-05-08 15:39:26 -0400

    Make validation parsing more efficient

--- parsing.py
+++ parsing.py
@@ -2,9 +2,9 @@
 Module for parsing functions
 '''
 
-# BEST_VAL_MARK = 'Best validation acc:'
-# LAST_VAL_MARK = 'Validation list:'
-# VAL_LIST_MARK = 'Validation list:'
+BEST_VAL_MARK = 'Best validation acc:'
+LAST_VAL_MARK = 'Validation list:'
+VAL_LIST_MARK = 'Validation list:'
 
 def parse_validations(filename):
     '''
@@ -16,11 +16,11 @@ def parse_validations(filename):
     last_val_acc = -1
     with open(filename, 'r') as f:
         for line in f:
-            if 'Validation list:' in line:
+            if line.startswith(VAL_LIST_MARK):
                 val_list_str = line.split(':')[1].strip().lstrip('[').rstrip(']')
                 val_list = [float(acc) for acc in val_list_str.split(',')]
-            elif 'Best validation acc:' in line:
+            elif line.startswith(BEST_VAL_MARK):
                 best_val_acc = float(line.split(':')[1].strip())
-            elif 'Last validation acc:' in line:
+            elif line.startswith(LAST_VAL_MARK):
                 last_val_acc = float(line.split(':')[1].strip())
     return best_val_acc, last_val_acc, val_list
