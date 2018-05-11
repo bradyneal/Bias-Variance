2fb18c828a4f982eb735d2d49bdfc3be79bb45e3
mattscicluna <mattcscicluna@gmail.com>
2018-05-11 10:52:15 -0400

    added change for batch training

--- run_cedar.sh
+++ run_cedar.sh
@@ -6,5 +6,7 @@ source $HOME/.bashrc
 export PYTHONUNBUFFERED=1
 echo Running on $HOSTNAME
 
+#source activate inf-path
+
 # Run the script
-python information-paths/matt_test.py
+python information-paths/matt_test.py $@
