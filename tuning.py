bb07fd0b55480eca67b0fd4b128d4dcb80cfce39
Brady Neal <bradyneal11@gmail.com>
2018-05-12 15:31:29 -0400

    Update tuning file to set parser format

--- tuning.py
+++ tuning.py
@@ -6,30 +6,35 @@ import matplotlib.pyplot as plt
 from credentials import SIGOPT_TOKEN    # note this is not pushed
 from sigopt import Connection
 
-i = 3
+i = 8
 if len(sys.argv) > 1:
     i = int(sys.argv[1])
     
-HIDDEN_SIZES = [5, 25, 100, 1E3, 5E3, 10E3, 20E3, 40E3, 80E3]
+HIDDEN_SIZES = [1, 2, 5, 25, 100, 500, 2500, 10000, 40000]
+# tuning SGD 40000 (500) hidden for 5 seeds: 175670, 175835
+
+# seed 0: 175028, 175029, 175083, 175033, 175035-175039 (175032 was unfinished for size 5)
+# seed 1-5: 175169, 175170, 175171, 175173, 175174, 175175, 175176, 175178, 175184
 NUM_HIDDEN = HIDDEN_SIZES[i]
 SEED = 0
 LOGLR_MIN = -5
 LOGLR_MAX = 0
-NUM_EVALS = 10
+NUM_EVALS = 20
+BASE = 10
 
 def val_cost(loglr, num_hidden, seed, maximize=True):
-    lr = 2 ** loglr
+    lr = BASE ** loglr
     print('learning rate:', lr)
-    val_acc, _ = DataModelComp(ShallowNet(num_hidden), epochs=5, log_interval=None,
+    val_acc, _ = DataModelComp(ShallowNet(num_hidden), epochs=500, log_interval=None,
                                run_i=seed, train_val_split_seed=seed, seed=seed,
                                bootstrap=True, batch_size=10, num_train_after_split=100,
-                               print_only_train_and_val_errors=False, print_all_errors=True, lr=lr, momentum=0.9,
-                               plot_curves=False).train()
+                               print_only_train_and_val_errors=True, print_all_errors=False, lr=lr, momentum=0.9,
+                               plot_curves=False, save_best_model=True).train()
     return val_acc if maximize else 1 - val_acc
 
 def tune_sigopt(num_hidden, seed, experiment_name=None, experiment_id=None):
     conn = Connection(client_token=SIGOPT_TOKEN)
-    
+
     if experiment_id is None:
         # Create experiment
         experiment = conn.experiments().create(
@@ -42,7 +47,7 @@ def tune_sigopt(num_hidden, seed, experiment_name=None, experiment_id=None):
         experiment_id = experiment.id
     else:
         print("Continuing experiment: https://sigopt.com/experiment/" + experiment_id)
-    
+
     # Run the Optimization Loop between 10x - 20x the number of parameters
     for _ in range(NUM_EVALS):
         suggestion = conn.experiments(experiment_id).suggestions().create()
@@ -52,14 +57,16 @@ def tune_sigopt(num_hidden, seed, experiment_name=None, experiment_id=None):
             suggestion=suggestion.id,
             value=value,
         )
-        
+
     return experiment_id
 
 if __name__ == '__main__':
-    exp_name='{} for hidden size {}'.format('SGD', NUM_HIDDEN)
-    exp_id = tune_sigopt(NUM_HIDDEN, SEED, experiment_name=exp_name, experiment_id='45171')
-    print('Experiment {} finished'.format(exp_id))
-    
+    for seed in range(5):
+        exp_name='{} for hidden size {} with seed {}'.format('SGD', NUM_HIDDEN, seed)
+        print('Starting exp:', exp_name)
+        exp_id = tune_sigopt(NUM_HIDDEN, seed, experiment_name=exp_name)
+        print('Experiment {} finished'.format(exp_id))
+
     # print('Hidden size:', num_hidden)
     # res = gp_minimize(val_cost, [loglr_range])
     
