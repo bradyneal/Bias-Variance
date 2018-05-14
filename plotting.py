71b81eede09a65c3c3997eb84ef5b60959c75f88
Vinayak Tantia <tantia.vinayak1@gmail.com>
2018-05-14 17:10:50 -0400

    Add legend, lable and linewidth options

--- plotting.py
+++ plotting.py
@@ -12,7 +12,7 @@ YSCALE_DEF = 'linear'
 GRID_DEF = False
 
 
-def plot_line_with_normal_errbars(x, y, y_std, xlabel=None, ylabel=None, title=None, filename=None, xscale=XSCALE_DEF, yscale=YSCALE_DEF, grid=GRID_DEF):
+def plot_line_with_normal_errbars(x, y, y_std, label=None, xlabel=None, ylabel=None, title=None, filename=None, xscale=XSCALE_DEF, yscale=YSCALE_DEF, grid=GRID_DEF, elinewidth=None):
     '''
     Plot figure with 95% confidence interval, according to Normal distirbution.
     Save figure if filename specified; otherwise, show the figure.
@@ -21,18 +21,16 @@ def plot_line_with_normal_errbars(x, y, y_std, xlabel=None, ylabel=None, title=N
         yerr = [1.96 * std for std in y_std]
     else:
         yerr = 1.96 * y_std
-    plt.figure()
-    plt.errorbar(x, y, yerr=yerr, capsize=CAPSIZE)
+    plt.errorbar(x, y, yerr=yerr, capsize=CAPSIZE, label=label, elinewidth=elinewidth)
     run_fig_extras(xlabel, ylabel, title, filename, xscale, yscale, grid)
 
 
-def plot_line_with_errbars(x, y, lowers, uppers, xlabel=None, ylabel=None, title=None, filename=None, xscale=XSCALE_DEF, yscale=YSCALE_DEF, grid=GRID_DEF):
+def plot_line_with_errbars(x, y, lowers, uppers, label=None, xlabel=None, ylabel=None, title=None, filename=None, xscale=XSCALE_DEF, yscale=YSCALE_DEF, grid=GRID_DEF, elinewidth=None):
     '''
     Plot figure with error bars specified by lowers and uppers (more general than above).
     Save figure if filename specified; otherwise, show the figure.
     '''
-    plt.figure()
-    plt.errorbar(x, y, yerr=[lowers, uppers], capsize=CAPSIZE)
+    plt.errorbar(x, y, yerr=[lowers, uppers], capsize=CAPSIZE, label=label, elinewidth=elinewidth)
     run_fig_extras(xlabel, ylabel, title, filename, xscale, yscale, grid)
 
 
@@ -50,8 +48,8 @@ def run_fig_extras(xlabel=None, ylabel=None, title=None, filename=None, xscale=X
     plt.xscale(xscale)
     plt.yscale(yscale)
     plt.grid(grid)
+    plt.legend(loc='upper right', fontsize='x-large')
     if filename:
-        plt.savefig(filename)
+        plt.savefig(filename, format='pdf')
     else:
         plt.show()
-    plt.close()
